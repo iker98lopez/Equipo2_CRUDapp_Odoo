3d31741cac996ff587c130147476df6ff570dbdf
iker lopez carrillo <iker98lopez@gmail.com>
2020-01-13 08:56:38 +0100

    First commit.

--- __manifest__.py
+++ __manifest__.py
@@ -3,14 +3,16 @@
     'name': "offerStats",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        This module will be used to offer stats about the most desired and
+        commented softwares of our app.""",
 
     'description': """
-        Long description of module's purpose
+        Module that calculates stats of our app based on the number of comments
+        each software has and the number of users that have wishlisted the same
+        software, and creates a new stat called "most popular softwares".
     """,
 
-    'author': "My Company",
+    'author': "Equipo 2",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
